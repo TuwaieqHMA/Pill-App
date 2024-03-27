cdb25dc397fb1a073511353fef0ee74cbd6753d6
unknown <kmf6001@gmail.com>
2024-03-27 21:01:59 +0300

    Fix Chat

--- lib/pages/ask_saed_page.dart
+++ lib/pages/ask_saed_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:pill_app/bloc/auth_bloc.dart';
 import 'package:pill_app/data_layer/home_data_layer.dart';
-import 'package:pill_app/helpers/extensions/screen_helper.dart';
 import 'package:pill_app/pages/test_pages/chat_page.dart';
 import 'package:pill_app/utils/colors.dart';
 import 'package:pill_app/utils/fonts.dart';
@@ -11,94 +10,106 @@ import 'package:pill_app/widgets/container_saed.dart';
 import 'package:pill_app/widgets/texet_saed.dart';
 
 class AskSaedPage extends StatelessWidget {
-  const AskSaedPage({super.key});
+  const AskSaedPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final locator = GetIt.I.get<HomeData>();
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Container(
-        width: context.getWidth() * 100,
-        height: context.getWidth() * 50,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment(0.0, 0.0),
-            colors: [deepGreenColor, signatureGreenColor],
-            tileMode: TileMode.clamp,
+      body: Center(
+        child: Container(
+          width: screenWidth,
+          height: screenHeight,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment(0.0, 0.0),
+              colors: [deepGreenColor, signatureGreenColor],
+              tileMode: TileMode.clamp,
+            ),
           ),
-        ),
-        child: Stack(children: [
-          const ContainerSaed1(),
-          const ContainerSaed3(),
-          const ContainerSaed2(),
-          Positioned(
-              top: 155,
-              right: 65,
-              child: Container(
-                height: 45,
-                width: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                  color: Colors.white,
-                ),
-                child: Center(
-                  child: BlocBuilder<AuthBloc, AuthState>(
-                    builder: (context, state) {
-                      return Text(
-                        'مرحبا ${locator.currentUser.name.split(" ")[0]}',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w800,
-                          fontFamily: poppinsFont,
-                          color: Colors.transparent.withOpacity(0.7),
-                        ),
-                      );
-                    },
+          child: Stack(
+            children: [
+              const ContainerSaed1(),
+              const ContainerSaed3(),
+              const ContainerSaed2(),
+              Positioned(
+                top: screenHeight * 0.18,
+                right: screenWidth * 0.13,
+                child: Container(
+                  height: screenHeight * 0.06,
+                  width: screenWidth * 0.35,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white,
+                  ),
+                  child: Center(
+                    child: BlocBuilder<AuthBloc, AuthState>(
+                      builder: (context, state) {
+                        return Text(
+                          'مرحبا ${locator.currentUser.name.split(" ")[0]}',
+                          style: TextStyle(
+                            fontSize: screenHeight * 0.024,
+                            fontWeight: FontWeight.w800,
+                            fontFamily: poppinsFont,
+                            color: Colors.transparent.withOpacity(0.7),
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
-              )),
-          Positioned(
-              top: 222,
-              left: 110,
-              child: Image.asset(
-                'assets/images/saed_logo.png',
-                height: 180,
-                width: 180,
-              )),
-          Positioned(
-              bottom: 288,
-              right: 215,
-              child: Image.asset(
-                'assets/images/hand.png',
-                height: 60,
-                width: 60,
-              )),
-          const Positioned(
-              bottom: 288,
-              right: 138,
-              child: TextSaed(
-                text: 'ساعد',
-                size: 38,
-              )),
-          const Positioned(
-              bottom: 190,
-              right: 175,
-              child: TextSaed(
-                text: 'أنا ساعد',
-                size: 24,
-              )),
-          const Positioned(
-              bottom: 150,
-              right: 100,
-              child: TextSaed(
-                text: 'كيف يمكنني مساعدتك؟',
-                size: 24,
-              )),
-          Positioned(
-              bottom: 70,
-              right: 160,
-              child: ElevatedButton(
+              ),
+              Positioned(
+                top: screenHeight * 0.25,
+                right: screenWidth * 0.25,
+                child: Image.asset(
+                  'assets/images/saed_logo.png',
+                  height: screenHeight * 0.24,
+                  width: screenHeight * 0.24,
+                ),
+              ),
+              Positioned(
+                top: screenHeight * 0.48,
+                right: screenWidth * 0.55,
+                child: Image.asset(
+                  'assets/images/hand.png',
+                  height: screenHeight * 0.07,
+                  width: screenHeight * 0.07,
+                ),
+              ),
+              Positioned(
+                  top: screenHeight * 0.48,
+                right: screenWidth * 0.33,
+                child: TextSaed(
+                  text: 'ساعد',
+                  size: screenHeight * 0.05,
+                ),
+              ),
+              Positioned(
+                bottom: screenHeight * 0.25,
+                right: screenWidth * 0.40,
+                child: TextSaed(
+                  text: 'أنا ساعد',
+                  size: screenHeight * 0.030,
+                ),
+              ),
+              Positioned(
+                 bottom: screenHeight * 0.16,
+                right: screenWidth * 0.24,
+                child: TextSaed(
+                  text: 'كيف يمكنني مساعدتك؟',
+                  size: screenHeight * 0.030,
+                ),
+              ),
+              Positioned(
+                bottom: screenHeight * 0.075,
+                right: screenWidth * 0.39,
+                child: ElevatedButton(
                   onPressed: () {
                     Navigator.push(
                       context,
@@ -107,17 +118,22 @@ class AskSaedPage extends StatelessWidget {
                   },
                   child: Image.asset(
                     'assets/images/chat_button.png',
-                    height: 50,
-                    width: 50,
-                  ))),
-          const Positioned(
-              bottom: 33,
-              right: 172,
-              child: TextSaed(
-                text: 'انقر للتحدث',
-                size: 16,
-              )),
-        ]),
+                    height: screenHeight * 0.05,
+                    width: screenHeight * 0.05,
+                  ),
+                ),
+              ),
+              Positioned(
+                     bottom: screenHeight * 0.040,
+                right: screenWidth * 0.42,
+                child: TextSaed(
+                  text: 'انقر للتحدث',
+                  size: screenHeight * 0.016,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/pages/test_pages/chat_page.dart
+++ lib/pages/test_pages/chat_page.dart
@@ -21,88 +21,99 @@ final translator = GoogleTranslator();
         }
       },
       builder: (context, state) {
-          return Scaffold(
-            backgroundColor: whiteColor,
-          body: DashChat(
-            inputOptions:  InputOptions(
-              sendButtonBuilder: (Function onSend) {
-            return  InkWell(
-              onTap: (){
-             onSend();
-              },
-                child: Container(
-                  height: 40,
-                  width: 40,
-                  decoration: BoxDecoration(
-                    color: goldColor,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: const Icon(
-                    Icons.send,
-                    color: Colors.white,
-                  ),
-                ),
-              );
-            },
-            inputDecoration: InputDecoration(
-         hintText: "...اكتب رسالة",
-    hintStyle: const TextStyle(color: Colors.grey), 
-    filled: true, 
-    fillColor: Colors.white, // Background color of the text field
-  
-    border: OutlineInputBorder( // Define the border of the text field
-      borderRadius: BorderRadius.circular(25),
-      borderSide: BorderSide.none,)
-           )),
-            typingUsers: locator.typingList,
-              messageOptions: MessageOptions(
-                maxWidth: MediaQuery.of(context).size.width * .5,
-                showCurrentUserAvatar: true,
-                currentUserContainerColor: glassGreyColor,
-                borderRadius: 15,
-                containerColor: glassGreyColor,
-                textColor: blackColor,
-                currentUserTextColor: blackColor,
-                showTime: true,
-                onLongPressMessage: (message) {
-                  context.read<ChatBloc>().add(RemoveChatEvent(messageToRemove: message, oldMessageList: locator.chatLog));
+          return Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [deepGreenColor, signatureGreencColor2, signatureGreenColor],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter),
+            ),
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+            body: DashChat(
+              inputOptions:  InputOptions(
+                sendButtonBuilder: (Function onSend) {
+              return  InkWell(
+                onTap: (){
+               onSend();
                 },
-                avatarBuilder: (p0, onPressAvatar, onLongPressAvatar) {
-                  return ClipRRect(
-                    borderRadius: BorderRadius.circular(40),
-                    child: Image.network(
-                      p0.profileImage!,
-                      width: 60,
-                      height: 60,
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      color: goldColor,
+                      borderRadius: BorderRadius.circular(30),
                     ),
-                  );
-                },
-              ),
-              currentUser: locator.user,
-onSend: (ChatMessage sentMessage) async {
-  if (sentMessage.text.startsWith("/Image:") || sentMessage.text.startsWith("image:")){
-    sentMessage.text = sentMessage.text.replaceAll("/image:", "").replaceAll("/Image:", "");
-    context.read<ChatBloc>().add(CreateImageEvent(textMessage: sentMessage));
-  } else {
-    final translatedText = await translator.translate(sentMessage.text, to: 'ar');
-    sentMessage.text = translatedText.text;
-    context.read<ChatBloc>().add(SendMessageEvent(typingUser: locator.bot, oldMessageList: locator.chatLog, textMessage: sentMessage, oldUserList: locator.typingList, sentMessage: null));
-  }
-},
-              messages: locator.chatLog),
-               floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        backgroundColor: glassGreyColor,
-        child: Image.asset(
-          'assets/icons/right.png',
-          height: 28,
-          width: 28,
-        ),
-      ),
-        );
+                    child: const Icon(
+                      Icons.send,
+                      color: Colors.white,
+                    ),
+                  ),
+                );
+              },
+              inputDecoration: InputDecoration(
+                     hintText: "...اكتب رسالة",
+                hintStyle: const TextStyle(color: Colors.grey), 
+                filled: true, 
+                fillColor: Colors.white, // Background color of the text field
+              
+                border: OutlineInputBorder( // Define the border of the text field
+                  borderRadius: BorderRadius.circular(25),
+                  borderSide: BorderSide.none,)
+             )),
+              typingUsers: locator.typingList,
+                messageOptions: MessageOptions(
+                  maxWidth: MediaQuery.of(context).size.width * 0.5,
+                  showCurrentUserAvatar: true,
+                  currentUserContainerColor: glassGreyColor,
+                  borderRadius: 15,
+                  containerColor: glassGreyColor,
+                  textColor: blackColor,
+                  currentUserTextColor: blackColor,
+                  showTime: true,
+                  onLongPressMessage: (message) {
+                    context.read<ChatBloc>().add(RemoveChatEvent(messageToRemove: message, oldMessageList: locator.chatLog));
+                  },
+                  avatarBuilder: (p0, onPressAvatar, onLongPressAvatar) {
+                    return ClipRRect(
+                      borderRadius: BorderRadius.circular(40),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10,right: 10),
+                        child: Image.network(
+                          p0.profileImage!,
+                          width: 60,
+                          height: 60,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                currentUser: locator.user,
+            onSend: (ChatMessage sentMessage) async {
+              if (sentMessage.text.startsWith("/Image:") || sentMessage.text.startsWith("image:")){
+                sentMessage.text = sentMessage.text.replaceAll("/image:", "").replaceAll("/Image:", "");
+                context.read<ChatBloc>().add(CreateImageEvent(textMessage: sentMessage));
+              } else {
+                final translatedText = await translator.translate(sentMessage.text, to: 'ar');
+                sentMessage.text = translatedText.text;
+                context.read<ChatBloc>().add(SendMessageEvent(typingUser: locator.bot, oldMessageList: locator.chatLog, textMessage: sentMessage, oldUserList: locator.typingList, sentMessage: null));
+              }
+            },
+                messages: locator.chatLog),
+                 floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+                  floatingActionButton: FloatingActionButton(
+                    onPressed: () {
+            Navigator.pop(context);
+                    },
+                    backgroundColor: glassGreyColor,
+                    child: Image.asset(
+            'assets/icons/right.png',
+            height: 28,
+            width: 28,
+                    ),
+                  ),
+                    ),
+          );
         
       },
     );
